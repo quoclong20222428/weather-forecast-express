1f3c2af4749b47079cc3567f3eb27bf9994e7b55
Huynh Hiep Pham <huynhhiep1223@gmail.com>
2025-10-28 19:21:50 +0700

    Remove redundant JS files

--- src/middleware/index.js
+++
@@ -1,5 +0,0 @@
-export { HttpError } from "./httpError.js";
-export { errorHandler } from "./errorHandler.js";
-export { notFoundHandler } from "./notFoundHandler.js";
-export { requestLogger } from "./requestLogger.js";
-export { cacheWeatherByCityNameMiddleware, cacheWeatherByCityIdMiddleware, cacheWeatherByLatLonMiddleware } from "./cacheWeather.js";
--- src/middleware/notFoundHandler.js
+++
@@ -1,4 +0,0 @@
-import { HttpError } from "./httpError.js";
-export const notFoundHandler = (req, _res, next) => {
-    next(new HttpError(404, `Route ${req.method} ${req.originalUrl} not found`));
-};
